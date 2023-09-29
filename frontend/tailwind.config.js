59c7ae3a523ffbc8447c72ba5efe9d86961cfc07
chahat30 <chahatba9876@gmail.com>
2023-09-29 13:45:40 +0530

    First commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],                                  //the content property is used to specify the source files that Tailwind CSS should scan to generate its utility classes. 
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+

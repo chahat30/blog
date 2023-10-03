54215842cb3d899bed986bd669fd335a067b691b
chahat30 <chahatba9876@gmail.com>
2023-10-03 19:54:01 +0530

    ArticleCard section done

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,8 +1,10 @@
 import logo from "../assets/Logo.svg";
 import heroImage from "../assets/HeroImage.svg";
+import postImage from "../assets/posts/post1.jpg";
+import postProfile from "../assets/posts/post-profile.svg";
 
 const images={
-    logo,heroImage
+    logo,heroImage,postImage,postProfile
 };
 
 export default images;
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import MainLayout from '../../components/MainLayout';
 import Hero from './container/Hero';
+import Articles from './container/Articles';
 
 export default function Home() {
   return (
     <div>
       <MainLayout>
         <Hero/>
-        </MainLayout>
+        <Articles/>
+      </MainLayout>
     </div>
   )
 }

7282ba26bdb18dd828b4db6757dc79505152e8ed
chahat30 <chahatba9876@gmail.com>
2023-10-04 19:49:54 +0530

    cta frontend

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -2,9 +2,10 @@ import logo from "../assets/Logo.svg";
 import heroImage from "../assets/HeroImage.svg";
 import postImage from "../assets/posts/post1.jpg";
 import postProfile from "../assets/posts/post-profile.svg";
+import ctaImage from '../assets/CtaImage.jpg';
 
 const images={
-    logo,heroImage,postImage,postProfile
+    logo,heroImage,postImage,postProfile,ctaImage
 };
 
 export default images;

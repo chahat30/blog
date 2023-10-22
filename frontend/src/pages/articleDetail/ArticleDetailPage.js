74e2874e09907a77db9832f04de04466a087301a
chahat30 <chahatba9876@gmail.com>
2023-10-22 23:43:34 +0530

    social media buttons- 1

--- frontend/src/pages/articleDetail/ArticleDetailPage.js
+++ frontend/src/pages/articleDetail/ArticleDetailPage.js
@@ -5,6 +5,7 @@ import {images} from '../../constants';
 import {Link} from 'react-router-dom';
 import SuggestedPosts from './container/SuggestedPosts';
 import CommentsContainer from '../../components/comments/CommentsContainer';
+import SocialShareButtons from '../../components/SocialShareButtons';
 
 const breadCrumbsData=[
     {name:"Home",link:"/"},
@@ -80,14 +81,20 @@ export default function ArticleDetailPage() {
               ut hendrerit metus.
             </p>
           </div>
-          <CommentsContainer className="mt-10" logginedUserId="a"/>
+          <CommentsContainer className="mt-10" logginedUserId="a" />
         </article>
-        <SuggestedPosts
-          header="Latest Article"
-          posts={postsData}
-          tags={tagsData}
-          className="mt-8 lg:mt-0 lg:max-w-xs"
-        />
+        <div>
+          <SuggestedPosts
+            header="Latest Article"
+            posts={postsData}
+            tags={tagsData}
+            className="mt-8 lg:mt-0 lg:max-w-xs"
+          />
+          <div className='mt-7'>
+            <h2 className='font-roboto font-medium text-dark-hard mb-4 md:text-xl'>Share on:</h2>
+            <SocialShareButtons url={encodeURI("https://github.com/chahat30")} title={encodeURIComponent("Github")}/>
+          </div>
+        </div>
       </section>
     </MainLayout>
   );

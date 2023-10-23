c9869f38e8b9af78771953467ae22b4fac1c9cde
chahat30 <chahatba9876@gmail.com>
2023-10-24 00:04:40 +0530

    backend basic setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app=express(); //instance of express package
+app.use(express.json()); //middleware wherein if frontend sends .json, it will be able to parse it
+
+app.get('/',(req,res)=>{
+    res.send("Server is running... ");
+})
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=> console.log(`server is running on port ${PORT}`));

181d23aec386286a913114cb67f2e3629001fd86
chahat30 <chahatba9876@gmail.com>
2024-01-10 18:11:18 +0530

    delete post controller

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router= express.Router();
-import {createPost, updatePost} from '../controllers/postControllers';
+import {createPost, deletePost, updatePost} from '../controllers/postControllers';
 import { authGuard, adminGaurd } from '../middleware/authMiddleware';
 
 router.post('/', authGuard, adminGaurd,createPost);
-router.put('/:slug', authGuard, adminGaurd,updatePost);
+router.route('/:slug').put( authGuard, adminGaurd,updatePost).delete(authGuard, adminGaurd,deletePost);
 
 export default router;

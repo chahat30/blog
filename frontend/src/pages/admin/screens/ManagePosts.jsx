d99d526f30252e84e259780bca11f862f8b1ce35
chahat30 <chahatba9876@gmail.com>
2024-02-03 23:04:21 +0530

    pagination frontend

--- frontend/src/pages/admin/screens/ManagePosts.jsx
+++ frontend/src/pages/admin/screens/ManagePosts.jsx
@@ -1,37 +1,59 @@
-import React ,{ useState }from "react";
-import { getAllPosts } from "../../../services/index/posts";
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 import { images, stables } from "../../../constants";
+import { getAllPosts } from "../../../services/index/posts";
+import { useEffect, useState } from "react";
+import Pagination from "../../../components/Pagination";
 
-export default function ManagePosts() {
+let isFirstRun = true;
 
+const ManagePosts = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
 
-  const {data:postsData, isLoading, isFetching, refetch} = useQuery({
-    queryFn : () => getAllPosts(searchKeyword,currentPage),
+  const {
+    data: postsData,
+    isLoading,
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryFn: () => getAllPosts(searchKeyword, currentPage),
     queryKey: ["posts"],
   });
 
-  const searchKeywordHandler = (e)=>{
-    const {value} = e.target;
-    setSearchKeyword(value);
-  }
+  console.log(postsData);
 
-  const submitSearchKeywordHandler = (e)=>{
-    e.preventDefault();
+  useEffect(() => {
+    if (isFirstRun) {
+      isFirstRun = false;
+      return;
+    }
     refetch();
-  }
+  }, [refetch, currentPage]);
+
+  const searchKeywordHandler = (e) => {
+    const { value } = e.target;
+    setSearchKeyword(value);
+  };
+
+  const submitSearchKeywordHandler = (e) => {
+    e.preventDefault();
+    setCurrentPage(1);
+    refetch();
+  };
 
   return (
     <div>
-      <h1 className="text-2xl font-semibold">Manage Posts</h1>
+      <h1 className="text-2xl font-semibold">Mange Posts</h1>
+
       <div className="w-full px-4 mx-auto">
         <div className="py-8">
           <div className="flex flex-row justify-between w-full mb-1 sm:mb-0">
             <h2 className="text-2xl leading-tight">Users</h2>
             <div className="text-end">
-              <form onSubmit={submitSearchKeywordHandler} className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
+              <form
+                onSubmit={submitSearchKeywordHandler}
+                className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0"
+              >
                 <div className=" relative ">
                   <input
                     type="text"
@@ -93,122 +115,91 @@ export default function ManagePosts() {
                         Loading...
                       </td>
                     </tr>
-                  ): (
+                  ) : (
                     postsData?.data.map((post) => (
                       <tr>
-                    <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0">
-                          <a href="/" className="relative block">
-                            <img
-                              alt={post.title}
-                              src={post?.image ? stables.UPLOAD_FOLDER_BASE_URL + post?.image : images.samplePostImage}
-                              className="mx-auto object-cover rounded-lg  w-10 aspect-square"
-                            />
-                          </a>
-                        </div>
-                        <div className="ml-3">
+                        <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                              <a href="/" className="relative block">
+                                <img
+                                  src={
+                                    post?.image
+                                      ? stables.UPLOAD_FOLDER_BASE_URL +
+                                        post?.image
+                                      : images.samplePostImage
+                                  }
+                                  alt={post.title}
+                                  className="mx-auto object-cover rounded-lg w-10 aspect-square"
+                                />
+                              </a>
+                            </div>
+                            <div className="ml-3">
+                              <p className="text-gray-900 whitespace-no-wrap">
+                                {post.title}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
                           <p className="text-gray-900 whitespace-no-wrap">
-                            {post.title}
+                            {post.categories.length > 0
+                              ? post.categories[0]
+                              : "Uncategorized"}
                           </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                      <p className="text-gray-900 whitespace-no-wrap">{post.categories.length > 0 ? post.categories[0] : "Uncategorized"}</p>
-                    </td>
-                    <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {new Date(post.createdAt).toLocaleDateString("en-US",{
-                          day: "numeric",
-                          month:"short",
-                          year:"numeric"
-                        })}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                      <div className="flex gap-x-2">
-                        {post.tags.length > 0 ? post.tags.map((tag,index)=>(
-                          <p>{tag}{post.tags.length-1 !== index && ','}</p>
-                        )): "No Tags"}
-                      </div>
-                    </td>
-                    <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                      <a
-                        href="/"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
+                        </td>
+                        <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                          <p className="text-gray-900 whitespace-no-wrap">
+                            {new Date(post.createdAt).toLocaleDateString(
+                              "en-US",
+                              {
+                                day: "numeric",
+                                month: "short",
+                                year: "numeric",
+                              }
+                            )}
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                          <div className="flex gap-x-2">
+                            {post.tags.length > 0
+                              ? post.tags.map((tag, index) => (
+                                  <p>
+                                    {tag}
+                                    {post.tags.length - 1 !== index && ","}
+                                  </p>
+                                ))
+                              : "No tags"}
+                          </div>
+                        </td>
+                        <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                          <a
+                            href="/"
+                            className="text-indigo-600 hover:text-indigo-900"
+                          >
+                            Edit
+                          </a>
+                        </td>
+                      </tr>
                     ))
                   )}
-                  
                 </tbody>
               </table>
-              <div className="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
-                <div className="flex items-center">
-                  <button
-                    type="button"
-                    className="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
-                  >
-                    <svg
-                      width="9"
-                      fill="currentColor"
-                      height="8"
-                      className=""
-                      viewBox="0 0 1792 1792"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
-                    </svg>
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 "
-                  >
-                    1
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                  >
-                    2
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-gray-600 bg-white border-t border-b hover:bg-gray-100"
-                  >
-                    3
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                  >
-                    4
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
-                  >
-                    <svg
-                      width="9"
-                      fill="currentColor"
-                      height="8"
-                      className=""
-                      viewBox="0 0 1792 1792"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
+              {!isLoading && (
+                <Pagination
+                  onPageChange={(page) => setCurrentPage(page)}
+                  currentPage={currentPage}
+                  totalPageCount={JSON.parse(
+                    postsData?.headers?.["x-totalpagecount"] || 1
+                  )}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default ManagePosts;

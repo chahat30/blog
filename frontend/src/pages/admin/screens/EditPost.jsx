7d75d25642f85d5e9af22fa7e0302f2593d4ae90
chahat30 <chahatba9876@gmail.com>
2024-02-18 22:11:59 +0530

    post categories (frontend) in edit post

--- frontend/src/pages/admin/screens/EditPost.jsx
+++ frontend/src/pages/admin/screens/EditPost.jsx
@@ -9,6 +9,14 @@ import { AiOutlineCamera } from "react-icons/ai";
 import toast from 'react-hot-toast';
 import { useSelector} from 'react-redux';
 import Editor from '../../../components/editor/Editor';
+import MultiSelectTagDropdown from '../components/header/MultiSelectTagDropdown';
+import { getAllCategories } from '../../../services/index/postCategories';
+import { categoryToOption, filterCategories } from '../../../utils/multiSelectTagUtils';
+
+const promiseOptions = async (inputValue) =>{
+    const categoriesData = await getAllCategories();
+    return filterCategories(inputValue,categoriesData);
+}
 
 export default function EditPost() {
 
@@ -18,6 +26,7 @@ export default function EditPost() {
     const[initialPhoto, setInitialPhoto] = useState(null);
     const [photo,setPhoto] = useState(null);
     const [body,setBody] = useState(null);
+    const [categories, setCategories] = useState(null);
    
     const { data, isLoading, isError } = useQuery({
         queryFn: () => getSinglePost({ slug }),
@@ -41,6 +50,7 @@ export default function EditPost() {
     useEffect(()=>{
         if(!isLoading && !isError){
             setInitialPhoto(data?.photo);
+            setCategories(data.categories.map((item)=> item._id));
         }
     },[data, isError, isLoading]);
 
@@ -63,7 +73,7 @@ export default function EditPost() {
             const picture = await urlToObject(stables.UPLOAD_FOLDER_BASE_URL + data?.photo);
             updatedData.append("postPicture",picture);
         }
-        updatedData.append("document",JSON.stringify({body}));
+        updatedData.append("document",JSON.stringify({body, categories}));
         mutateUpdatePostDetail({updatedData,slug,token: userState.userInfo.token});
     }
 
@@ -74,48 +84,95 @@ export default function EditPost() {
         }
     }
 
+    let isPostDataLoadead = !isLoading && !isError;
+
   return (
     <div>
-      {isLoading ? (<ArticleDetailSkeleton/>) : isError ? (<ErrorMessage message="Couldn't fetch post"/>):
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-        <article className="flex-1">
-          <label htmlFor="postPicture" className='w-full cursor-pointer'>
-            {photo ? (<img src={URL.createObjectURL(photo)} alt={data?.title} className='rounded-xl w-full'/>) : initialPhoto ?
-                <img src={stables.UPLOAD_FOLDER_BASE_URL + data?.photo} alt={data?.title} className='rounded-xl w-full'/> : (
-                    <div className='w-full min-h-[200px] bg-blue-50/50 flex justify-center items-center'>
-                        <AiOutlineCamera className='w-7 h-auto text-primary'/>
-                    </div>
-                )
-        }
-          </label>
-          <input type="file" className='sr-only' id='postPicture' onChange={handleFileChange}/>
-          <button type='button' onClick={handleDeleteImage} className='w-fit bg-red-500 text-sm text-white font-semibold rounded-lg px-2 py-1 mt-5'>Delete Image</button>
-          <div className='mt-4 flex gap-2'>
-          {data?.categories.map((category) => (
-            <Link
-            className="text-primary text-sm font-roboto inline-block mt-4 md:text-base"
-            to={`/blog?category=${category.name}`}
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Couldn't fetch post" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <label htmlFor="postPicture" className="w-full cursor-pointer">
+              {photo ? (
+                <img
+                  src={URL.createObjectURL(photo)}
+                  alt={data?.title}
+                  className="rounded-xl w-full"
+                />
+              ) : initialPhoto ? (
+                <img
+                  src={stables.UPLOAD_FOLDER_BASE_URL + data?.photo}
+                  alt={data?.title}
+                  className="rounded-xl w-full"
+                />
+              ) : (
+                <div className="w-full min-h-[200px] bg-blue-50/50 flex justify-center items-center">
+                  <AiOutlineCamera className="w-7 h-auto text-primary" />
+                </div>
+              )}
+            </label>
+            <input
+              type="file"
+              className="sr-only"
+              id="postPicture"
+              onChange={handleFileChange}
+            />
+            <button
+              type="button"
+              onClick={handleDeleteImage}
+              className="w-fit bg-red-500 text-sm text-white font-semibold rounded-lg px-2 py-1 mt-5"
             >
-            {category.name}
-            </Link>
-          ))}
-          </div>
-          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
-            {data?.title}
-          </h1>
-          <div className="w-full">
-            {!isLoading && !isError && (
-                <Editor content={data?.body} editable={true} onDataChange={(data)=> {
+              Delete Image
+            </button>
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  className="text-primary text-sm font-roboto inline-block mt-4 md:text-base"
+                  to={`/blog?category=${category.name}`}
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="my-5">
+              {isPostDataLoadead && (
+                <MultiSelectTagDropdown
+                  loadOptions={promiseOptions}
+                  onChange={(newValue) => 
+                    setCategories(newValue.map((item)=> item.value))
+                  }
+                  defaultValue={data.categories.map(categoryToOption)}
+                />
+              )}
+            </div>
+            <div className="w-full">
+              {isPostDataLoadead && (
+                <Editor
+                  content={data?.body}
+                  editable={true}
+                  onDataChange={(data) => {
                     setBody(data);
-                }}/>
-            )}
-          </div>
-          <button disabled={isLoadingUpdatePostDetail} type='button' onClick={handleUpdatePost} className='w-full bg-green-500 text-white font-semibold rounded-lg px-4 py-2 disabled:cursor-not-allowed disabled:opacity-70'>
-            Update Post
-          </button>
-        </article>
-      </section>
-      }
+                  }}
+                />
+              )}
+            </div>
+            <button
+              disabled={isLoadingUpdatePostDetail}
+              type="button"
+              onClick={handleUpdatePost}
+              className="w-full bg-green-500 text-white font-semibold rounded-lg px-4 py-2 disabled:cursor-not-allowed disabled:opacity-70"
+            >
+              Update Post
+            </button>
+          </article>
+        </section>
+      )}
     </div>
-  )
+  );
 }

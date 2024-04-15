27600ca73734d339a69c19221ced0e85d3a1353f
chahat30 <chahatba9876@gmail.com>
2024-04-15 17:06:43 +0530

    get all users and delete user

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ import {sign} from 'jsonwebtoken';
 const UserSchema= new Schema({
     avatar: { type: String, default: "" },
     name: { type: String, required: true},
-    email: { type: String, required: true},
+    email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
     verified: { type: Boolean, default: false},
     verificationCode: { type: String, required: false},
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 const router= express.Router();
-import { loginUser, registerUser, updateProfile, userProfile, updateProfilePicture} from '../controllers/userControllers';
-import { authGuard } from '../middleware/authMiddleware';
+import { loginUser, registerUser, updateProfile, userProfile, updateProfilePicture, getAllUsers, deleteUser} from '../controllers/userControllers';
+import { adminGaurd, authGuard } from '../middleware/authMiddleware';
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', authGuard, userProfile);
 router.put('/updateProfile',authGuard ,updateProfile);
 router.put('/updateProfilePicture', authGuard, updateProfilePicture);
+router.get('/',authGuard,adminGaurd,getAllUsers);
+router.delete('/:userId',authGuard, adminGaurd, deleteUser);
 
 export default router;
